9cb4d99c9e01690804daeab270e096ea480d4293
Ajinkya <patilajink2005@gmail.com>
2025-01-26 15:55:19 +0530

    Logical operator

---
+++ logical_op.py
@@ -0,0 +1,20 @@
+# AND operator
+
+print(True and True)   #output --> True
+print(True and False)  #output --> False    
+print(False and True)  #output --> False    
+print(False and False) #output --> False
+
+# OR operator   
+
+print(True or True)    #output --> True 
+print(True or False)   #output --> True 
+print(False or True)   #output --> True 
+print(False or False)  #output --> False
+
+# NOT operator
+
+print(not True)        #output --> False
+print(not False)       #output --> True
+print(not True and True) #output --> False
+print(not True or True)  #output --> True
