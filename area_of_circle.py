7f6d500732196cbc18d599683998b551110072cd
Ajinkya <patilajink2005@gmail.com>
2025-01-27 18:14:30 +0530

    practice que

---
+++ area_of_circle.py
@@ -0,0 +1,11 @@
+#taking input of radius
+radius = int(input("Enter the radius of circle : "))
+
+# formula = pi r^2
+area   = 3.14 * radius * radius
+
+#answer
+print("Area of circle is", area)  
+
+# takin r = 15
+#output --> Area of circle is 706.5
---
+++ area_of_triange.py
@@ -0,0 +1,12 @@
+#taking area of traingle
+breadth = int(input("Enter the breadth of triangle (b) : "))
+height = int(input("Enter the height of triangle (h) : "))
+
+# formula pof triangle
+area = 0.5 * breadth * height
+
+#answer
+print("Area of triangle is : ", area)
+
+#input -> b=10, h=20
+#output -> 100
