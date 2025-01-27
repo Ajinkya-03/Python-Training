885a2c9f608c4ec74fccc72cd5dc421b12d2a671
Ajinkya <patilajink2005@gmail.com>
2025-01-27 18:26:08 +0530

    global variable

---
+++ global_override.py
@@ -0,0 +1,15 @@
+x = "easy"
+
+def newfunc():
+    global x
+    x = "difficult"
+    print("Python is : ",x)
+
+newfunc()
+print("Python is : ",x)
+
+#since the value of x is beign override by the global value of x therefore the output will be "difficult"
+
+#output :
+# Python is :  difficult
+#Python is :  difficult
---
+++ global_var.py
@@ -0,0 +1,14 @@
+x = "easy"
+
+def newfunc():
+    x = "difficult"
+    print("Python is : ",x)
+
+newfunc()
+print("Python is : ",x)
+
+#since the value of x is diff for both set thus diff output will print
+
+#output :
+# Python is :  difficult
+#Python is :  easy
