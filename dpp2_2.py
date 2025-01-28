328c86f42309691618d4393bf1886daed4157b5e
Ajinkya <patilajink2005@gmail.com>
2025-01-28 18:17:21 +0530

    DPP practice

---
+++ dpp2_2.py
@@ -0,0 +1,10 @@
+#taking age as input
+age = float(input("Enter your age: "))
+
+#check if eligible
+if age >= 18:
+    print("You are eligible to vote")
+
+#not eligible
+else:
+    print("You are not eligible to vote")
---
+++ dpp2_3.py
@@ -0,0 +1,7 @@
+check = input("Enter a char : ")
+
+if (check == "a" ,"e" , "i" , "o" , "u" , "A" , "E" , "I" , "O" , "U"):
+    print("Vowel")
+
+else:
+    print("Consonant")  
