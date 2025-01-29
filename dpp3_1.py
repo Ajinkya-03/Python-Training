61dd032c4fb273c0ea626864416b1ea60f33547f
Ajinkya <patilajink2005@gmail.com>
2025-01-29 17:11:37 +0530

    practice

---
+++ dpp3_1.py
@@ -0,0 +1,8 @@
+# Taking input from the user
+num = int(input("Enter a number to print its multiplication table : "))
+i = 1
+
+# Printing the multiplication table using a while loop
+while (i <= 10):
+    print(num , "x" , i , " = " ,num * i)
+    i += 1
