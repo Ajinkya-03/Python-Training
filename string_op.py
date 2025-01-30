0fe911245366b3908b2de5a103eaffcda902690f
Ajinkya <patilajink2005@gmail.com>
2025-01-30 17:26:24 +0530

    string op

---
+++ string_op.py
@@ -0,0 +1,12 @@
+str = ("Hello World")
+
+#operations on stirng
+
+print(str)                         #Hello world
+print(str[0])                      #H
+print(str[2:8])                    #llo World
+print(str[2:])                     #llo World
+print(str * 2)                     #Hello WorldHello World
+print(str + " Over there")         #Hello WorldOver there
+print(str[::-1])                   #dlroW olleH
+
